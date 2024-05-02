1a03df074220ac61e329fffcd2d7714eb89f722e
varshabugad <varshabugad@gmail.com>
2024-05-02 14:55:27 +0530

    comment by asian guy

--- src/main/java/VarshaKharage/ExtentReports/App.java
+++ src/main/java/VarshaKharage/ExtentReports/App.java
@@ -3,5 +3,7 @@ package VarshaKharage.ExtentReports;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println("Hello World2!");
+
   }
 }
